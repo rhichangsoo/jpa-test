e74f9e6dfe94c9d3d24af6cbac3b3cbdea4e8a09
rhichangsoo <rhichangsoo@192.168.219.108>
2019-05-18 06:16:31 +0900

    field name change

--- src/executor/MainClass.java
+++ src/executor/MainClass.java
@@ -8,11 +8,11 @@ import domain.product.service.ProductService;
 
 public class MainClass extends UpperExecutor {
 	public static void main(String[] args) {
-		ProductService productService = (ProductService)ctx.getBean("productService");
+		ProductService productService = (ProductService)acac.getBean("productService");
 		List<Product> products = productService.getProductByName("신짱");
 		products.forEach(System.out::println);
 		
-		OrderService orderService = (OrderService)ctx.getBean("orderService");
-		ctx.close();
+		OrderService orderService = (OrderService)acac.getBean("orderService");
+		acac.close();
 	}
 }
--- src/executor/UpperExecutor.java
+++ src/executor/UpperExecutor.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class UpperExecutor {
 	protected static final String CONFIG_PACKAGES[] = {"config","domain/**"};
-	protected static AnnotationConfigApplicationContext ctx = null;
+	protected static AnnotationConfigApplicationContext acac = null;
 	static {
-		ctx = new AnnotationConfigApplicationContext();
-		ctx.scan(CONFIG_PACKAGES);
-		ctx.refresh();		
+		acac = new AnnotationConfigApplicationContext();
+		acac.scan(CONFIG_PACKAGES);
+		acac.refresh();		
 	}
 }
