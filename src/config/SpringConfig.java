7b6d4ebaebbd81291090cfe70c60a4f2c08639f8
changsoo rhi <changsoo.rhi@gmail.com>
2025-03-14 17:38:29 +0900

    Update SpringConfig.java

--- src/config/SpringConfig.java
+++ src/config/SpringConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+//abc
 
 @Configuration
 @ComponentScan(basePackages = {"domain"})
